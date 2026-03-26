8a52bad2f976b08f766f48493129439f364fc3a1
Raunak Raut <raunakraut01@gmail.com>
2026-03-26 23:08:41 +0530

    Add files via upload

---
+++ assignment_2.py
@@ -0,0 +1,47 @@
+#question 1
+a=40
+b=50
+c=a+b
+print(c)
+
+#question2
+a=20
+print("integer value=",a)
+print("type of a =", type(a))
+
+b=10.2
+print("\n float value=",b)
+print("type of b=", type(b))
+
+c="raunak"
+print("\n string value=", c)
+print("type of c=", type(c))
+
+d=True
+print("\n Boolean value=", d)
+print("type of d=", type(d))
+
+#question3
+a=10
+print("\n type of a=", type(a))
+
+b=10.5
+c=a+b
+print("\n addition is =", c)
+print("type is=", type(c))
+
+d=a*b
+print("\n multiplication is =",d)
+print("type is=", type(d))
+
+#question4 
+a=1
+b=0
+c=a and b
+print(c)
+
+d=a or b
+print(d)
+
+e=not a
+print(e)
