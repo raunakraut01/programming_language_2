8ba92f6ba4f7dd7ee36cd6ea49b30fae320f6fc5
Raunak Raut <raunakraut01@gmail.com>
2026-03-26 23:07:31 +0530

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,41 @@
+# Python Program to Demonstrate Different Data Types
+
+# Integer
+a = 25
+print("Integer value:", a)
+print("Type:", type(a))
+
+# Float
+b = 10.75
+print("\nFloat value:", b)
+print("Type:", type(b))
+
+# String
+c = "Hello Python"
+print("\nString value:", c)
+print("Type:", type(c))
+
+# Boolean
+d = True
+print("\nBoolean value:", d)
+print("Type:", type(d))
+
+# List
+e = [1, 2, 3, 4, 5]
+print("\nList value:", e)
+print("Type:", type(e))
+
+# Tuple
+f = (10, 20, 30)
+print("\nTuple value:", f)
+print("Type:", type(f))
+
+# Dictionary
+g = {"Name": "Rahul", "Age": 20}
+print("\nDictionary value:", g)
+print("Type:", type(g))
+
+# Set
+h = {1, 2, 3, 4}
+print("\nSet value:", h)
+print("Type:", type(h))
