d05329e77b7a95a2031b8f7c8cc13fb1945e7e39
Raunak Raut <raunakraut01@gmail.com>
2026-04-03 13:20:12 +0530

    Add files via upload

---
+++ assignment_3.py
@@ -0,0 +1,34 @@
+#question 1
+a=10
+b=20
+c=30
+if a>b and a>c:
+    print("a is the greatest")
+elif b>a and b>c:
+    print("b is the greatest")
+else:
+    print("c is the greatest")
+
+#question2
+a=1
+if a%2==0:
+    print("a is even")
+else:
+    print("a is odd")
+
+#question3
+char=input("enter a character=")
+if char.isupper():
+    print("{char}is uppercase")
+    
+else:
+    print("{char}is lowercase")
+
+
+ #question4
+a=input("enter a value=")
+if a.isdigit():
+    print("a is a number")
+
+else:
+    print("a is a character")
