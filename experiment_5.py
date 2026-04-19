a5a6ec2871ae9c4b2fd55ed04f36287934f4d46b
Raunak Raut <raunakraut01@gmail.com>
2026-04-19 22:09:03 +0530

    Add files via upload

---
+++ experiment_5.py
@@ -0,0 +1,14 @@
+#question1
+a="python"
+print(len(a))
+
+#question2 
+A= input("enter a string=")
+B=A[:2] + A[-2:]
+print(B)
+
+#question3
+A=input("enter 1st string=")
+B=input("enter 2nd string=")
+c=A + B
+print(c)
